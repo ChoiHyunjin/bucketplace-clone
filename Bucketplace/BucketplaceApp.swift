66a52357ee217d15278db293e48be57f8edd8c27
Choihyunjin <guswls3373@gmail.com>
2023-04-16 12:58:22 +0900

    chore: project setup

---
+++ Bucketplace/BucketplaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketplaceApp.swift
+//  Bucketplace
+//
+//  Created by Tophy on 2023/04/16.
+//
+
+import SwiftUI
+
+@main
+struct BucketplaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
